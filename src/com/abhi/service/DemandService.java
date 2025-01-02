929f38af6cfc8d835f11146c6b0e850115e61049
Ahijit <abhijitrupnawar@gmail.com>
2025-01-02 14:55:45 +0530

    i am adding my shopping website code on the gihub

---
+++ src/com/abhi/service/DemandService.java
@@ -0,0 +1,17 @@
+package com.abhi.service;
+
+import java.util.List;
+
+import comabhi.beans.DemandBean;
+
+public interface DemandService {
+
+	public boolean addProduct(String userId, String prodId, int demandQty);
+
+	public boolean addProduct(DemandBean userDemandBean);
+
+	public boolean removeProduct(String userId, String prodId);
+
+	public List<DemandBean> haveDemanded(String prodId);
+
+}
